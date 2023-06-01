e9b35a2a46b1b5e4e15871068c8713576fd3f105
luismelero <lmelerojareno@gmail.com>
2023-06-01 14:08:51 +0200

    Initial commit

---
+++ src/PaqC01/Puerto.java
@@ -0,0 +1,52 @@
+package PaqC01;
+
+import java.io.Serializable;
+
+public class Puerto implements Serializable {
+    private Hub[] puerto;
+
+    public Puerto() {
+        this.puerto = new Hub[3];
+    }
+
+    public Hub[] getPuerto() {
+        return puerto;
+    }
+
+    public void setPuerto(Hub[] puerto) {
+        this.puerto = puerto;
+    }
+
+    public boolean apilaContenedor(int hub_apilar, Contenedor c) {
+        return puerto[hub_apilar].apilaContenedor(c);
+    }
+
+    public Contenedor desapilaContenedor(int hub_desapilar, int columna) {
+        return puerto[hub_desapilar].desapilaContenedor(columna);
+    }
+
+    public Contenedor mostrarDatos(int hub_mostrar, int numIdentf) {
+        return puerto[hub_mostrar].mostrarDatos(numIdentf);
+    }
+
+    public String toStringHUB(int hub_mostrar) {
+        return puerto[hub_mostrar].toStringHUB();
+    }
+
+    public int contenedoresPorPais(String pais) {
+        int total = 0;
+
+        for (int i = 0; i < puerto.length; i++) {
+            total += puerto[i].contenedoresPorPais(pais);
+        }
+
+        return total;
+    }
+    public int buscaPrioridadPuerto(int prioridad){
+        int x=0;
+        for (int i = 0; i < puerto.length; i++) {
+            if(puerto[i].buscarPrioridad(1)==prioridad)x++;
+        }
+        return x;
+    }
+}
